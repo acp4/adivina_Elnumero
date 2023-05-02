3be0fd2185cae8713d4a5e137ce3c5a2920d3060
Abraham Castillo <abrahamcastillo090@gmail.com>
2023-05-02 09:22:00 -0600

    ci:correcion de varibales

---
+++ assets/js/adivina.js
@@ -0,0 +1,51 @@
+const incrementa = document.getElementById("incrementamas-btn");
+const disminuye = document.getElementById("incrementamenos-btn");
+const empieza = document.getElementById("empieza-btn");
+const termina = document.getElementById("termina-btn");
+const repite = document.getElementById("repetir-btn");
+const resultado = document.getElementById("resultado");
+let min = 1;
+let max = 100;
+let num = 0;
+
+
+function busquedaBinaria() {
+    num = parseInt((min + max) / 2);
+    document.getElementById("resultado").innerHTML = `¿Estás pensando en el número ${num}?`;
+
+}
+
+disminuye.addEventListener("click", function () {
+    max = num - 1;
+    busquedaBinaria();
+});
+
+incrementa.addEventListener("click", function () {
+    min = num + 1;
+    busquedaBinaria();
+});
+
+empieza.addEventListener("click", function () {
+    incrementa.removeAttribute("class");
+    disminuye.removeAttribute("class");
+    termina.removeAttribute("class");
+    incrementa.setAttribute("class", "col-lg-2 col-md-3 col-sm-5 btn btn-dark btn-lg");
+    disminuye.setAttribute("class", "col-lg-2 col-md-3 col-sm-5 btn btn-dark btn-lg");
+    termina.setAttribute("class", "col-lg-2 col-md-3 col-sm-5 btn btn-dark btn-lg");
+    empieza.setAttribute("class", "hide");
+})
+termina.addEventListener("click", function () {
+    document.getElementById("frase").innerHTML = `¡He ganado! estabas pensando en el ${num}`;
+    repite.removeAttribute("class");
+    repite.setAttribute("class", "col-lg-2 col-md-3 col-sm-5 btn btn-dark btn-lg");
+    incrementa.setAttribute("class", "hide");
+    disminuye.setAttribute("class", "hide");
+    termina.setAttribute("class", "hide");
+    empieza.setAttribute("class", "hide");
+    resultado.setAttribute("class", "hide");
+});
+
+repite.addEventListener("click", _ => {
+    location.reload();
+
+});
